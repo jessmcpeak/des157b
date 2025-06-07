331dbf9ce5a9a472c57ff25906b7172e0c39dd4f
jlmcpeak <you@youremail.com>
2025-06-07 09:37:45 -0700

    capstone start

--- capstone/development/version1/script.js
+++ capstone/development/version1/script.js
@@ -1,16 +1,4 @@
-const position = { x: 0, y: 0 }
-
-interact('.draggable').draggable({
-  listeners: {
-    start (event) {
-      console.log(event.type, event.target)
-    },
-    move (event) {
-      position.x += event.dx;
-      position.y += event.dy;
-
-      event.target.style.transform =
-        `translate(${position.x}px, ${position.y}px)`;
-    },
-  }
-});
+(function() {
+    `use strict`;
+    console.log(`running js`);
+})();
